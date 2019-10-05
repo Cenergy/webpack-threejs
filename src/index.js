830af083adffbeebb55c6161c26d4fd0b3fc2e57
Cenergy <673598118@qq.com>
2019-10-05 22:40:34 +0800

    update

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ const container = document.getElementById("content");
 const btn1 = new ButtonContent({ value: "第一个哦", fn: first });
 const btn2 = new ButtonContent({ value: "第2个哦", fn: second, id: "btn2" });
 
+// 默认执行第一个哦
+first();
 container.append(btn1);
 container.append(document.createElement("br"));
 container.append(btn2);
