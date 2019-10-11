c015f9ad9de5c145eabb63f56d2972e4d6eb85ad
Cenergy <673598118@qq.com>
2019-10-11 20:58:16 +0800

    add

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import first from "./test/first";
 import second from "./test/second";
+import third from "./test/third";
 import ButtonContent from "./utils/ceateButton";
 
 const container = document.getElementById("content");
 
 const btn1 = new ButtonContent({ value: "第一个哦", fn: first });
 const btn2 = new ButtonContent({ value: "第2个哦", fn: second, id: "btn2" });
+const btn3 = new ButtonContent({
+  value: "第3个哦",
+  fn: third,
+  id: "btn3"
+});
 
 // 默认执行第一个哦
 first();
 container.append(btn1);
 container.append(document.createElement("br"));
 container.append(btn2);
+container.append(document.createElement("br"));
+container.append(btn3);
---
+++ src/test/third.js
@@ -0,0 +1,13 @@
+import * as THREE from "three";
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+function hello() {
+  console.log("11111111111");
+}
+export default function third() {
+  // const gui = new dat.GUI();
+  // console.log("Go: second -> gui", gui);
+
+  $("#app").empty();
+  hello();
+}
